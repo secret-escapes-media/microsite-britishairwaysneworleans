a026ae6db9986e8d5f64555ce120a3b95bf70600
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-05-03 14:05:02 +0100

    site content

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,20 @@
 // general js for the project that wouldn't be a reuseable component
+
+
+///////////////////////////////////////
+//    Expand image credit
+///////////////////////////////////////
+
+$('.js-image-credit').each(function() {
+  // grab the credit text
+  var imageCredit = $(this).html();
+  // remove credit text and use a smaller label
+  $(this).html('Image Source').css('cursor', 'pointer');
+  // on click replace the label with the original credit text
+  $(this).on('click', function() {
+    $(this).html(imageCredit).css({
+      'cursor': 'auto',
+      'opacity': '1',  
+    });
+  });
+});
